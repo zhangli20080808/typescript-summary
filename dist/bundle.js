9ff73327d2f5e8585bfda8c9b0f5b60d1ffa65f8
zhangli20080808 <791492434@qq.com>
2021-03-08 23:07:20 +0800

    feat: 泛型类、泛型约束

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,7 +6,15 @@
       obj.length;
   }
   getType('1');
-  // 类中使用泛型
+  // 属性约束
+  // 1. T是一个对象类型 2.K是T中的一个属性
+  // keyof 表示取对象中所有的key属性
+  var getVal = function (obj, key) {
+      return obj[key];
+  };
+  getVal({ a: 1, b: 2 }, 'a');
+  // 泛型类 类中使用泛型
+  // 注意 泛型类指的是我们实例部分的类型  类的静态属性是不能使用的
   var MyArray = /** @class */ (function () {
       function MyArray() {
           this.arr = [];
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,25 @@ function createArray<T>(times: number, val: T) {
   }
   return result;
 }
+// 使用方式 2种 复杂场景可能需要手动传入
 let res = createArray<string>(3, 'abc'); // 不传入会自动推到
+
+// 类型推断
+let res2 = createArray(3, '111');
+
+// 泛型变量 可以使我们的T当做一部分使用 而不是整个类型的使用 灵活
+function ident<T>(arg: T): T {
+  return arg;
+}
+
+// 泛型函数类型
+let myIdent: <T>(arg: T) => T = ident;
+// 使用对象字面量形式定义
+interface GenericIdent<T> {
+  (arg: T): T;
+}
+let myIdent2: GenericIdent<number> = ident;
+
 // 泛型可以使用多个 比如有很多参数 每个参数都有类型
 
 //  交换类型 传入参数是个元组 [number,string] => [string,number]
@@ -62,22 +80,33 @@ let str3: T3 = { name: true };
 // 属性约束
 // 1. T是一个对象类型 2.K是T中的一个属性
 // keyof 表示取对象中所有的key属性
-const getVal = <T extends Object, K extends keyof T>(obj: T, key: K) => {};
+const getVal = <T extends Object, K extends keyof T>(obj: T, key: K) => {
+  return obj[key];
+};
 getVal({ a: 1, b: 2 }, 'a');
 
 type t1 = keyof any; // string | number | symbol
 type t2 = keyof (string | number); // "toString" | "valueOf"
 
-// 类中使用泛型
+// 泛型类 类中使用泛型
+// 注意 泛型类指的是我们实例部分的类型  类的静态属性是不能使用的
 class MyArray<T> {
   public arr: T[] = [];
+  public num!: T;
   add(v: T) {
     this.arr.push(v);
   }
+  set!: (x: T, y: T) => T;
 }
 
 let array = new MyArray<number>();
 array.add(1);
 array.add(2);
 array.add(3);
+
+// 类类型在工厂函数中的应用
+function create<T>(c: { new (): T }): T {
+  return new c();
+}
+
 export {};
