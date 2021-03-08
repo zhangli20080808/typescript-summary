2b70bf0372edf9102959c573b623da4850afdce9
zhangli20080808 <791492434@qq.com>
2021-03-08 22:41:04 +0800

    feat: 属性约束、类中使用泛型

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,10 +2,24 @@
   'use strict';
 
   // 泛型 就是当调用的时候来传入一个类型 先用一个标识来占位
-  var swap = function (tuple) {
-      return [tuple[0], tuple[1]];
-  };
-  swap([0, '1']);
+  function getType(obj) {
+      obj.length;
+  }
+  getType('1');
+  // 类中使用泛型
+  var MyArray = /** @class */ (function () {
+      function MyArray() {
+          this.arr = [];
+      }
+      MyArray.prototype.add = function (v) {
+          this.arr.push(v);
+      };
+      return MyArray;
+  }());
+  var array = new MyArray();
+  array.add(1);
+  array.add(2);
+  array.add(3);
 
 }());
 //# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -22,15 +22,62 @@ let res = createArray<string>(3, 'abc'); // 不传入会自动推到
 
 // 写到函数上的泛型 表示调用函数的时候 传入具体类型
 // 写在接口后面的 表示使用接口时传入的类型  MySwap<T, K>
-interface MySwap<T, K> {
-  (tuple: [T, K]): [K, T];
-}
-interface IArr<B> {
-  [key: number]: B;
-}
-const swap = <B>(tuple: IArr<B>): IArr<B> => {
-  return [tuple[0], tuple[1]];
-};
-let r = swap([0, '1']);
+// interface MySwap<T, K> {
+//   (tuple: [T, K]): [K, T];
+// }
+// interface IArr<B> {
+//   [key: number]: B;
+// }
+// const swap = <B>(tuple: IArr<B>): IArr<B> => {
+//   return [tuple[0], tuple[1]];
+// };
+// let r = swap([0, '1']);
 
+// 函数求和
+const sum = <T extends number>(a: T, b: T): T => {
+  return (a + b) as T;
+};
+sum(1, 2); // 1和2具备数字的能力，约束T是number类型
+//  extends 泛型约束 约束泛型的能力
+
+// 希望传入的数据 只要是带有length属性就可以 可以使字符串 数组各种把
+// 此处的extends不是继承的意思 是包含、约束的意思 T满足WithLen里面的条件
+type WithLen = { length: number };
+function getType<T extends WithLen>(obj: T) {
+  obj.length;
+}
+getType('1');
+
+// 默认泛型 不传递 默认给与类型
+interface DStr<T = string> {
+  name: T;
+}
+type T1 = DStr;
+type T2 = DStr<number>;
+type T3 = DStr<boolean>;
+let str1: T1 = { name: '123' };
+let str2: T2 = { name: 123 };
+let str3: T3 = { name: true };
+
+// 属性约束
+// 1. T是一个对象类型 2.K是T中的一个属性
+// keyof 表示取对象中所有的key属性
+const getVal = <T extends Object, K extends keyof T>(obj: T, key: K) => {};
+getVal({ a: 1, b: 2 }, 'a');
+
+type t1 = keyof any; // string | number | symbol
+type t2 = keyof (string | number); // "toString" | "valueOf"
+
+// 类中使用泛型
+class MyArray<T> {
+  public arr: T[] = [];
+  add(v: T) {
+    this.arr.push(v);
+  }
+}
+
+let array = new MyArray<number>();
+array.add(1);
+array.add(2);
+array.add(3);
 export {};
