65eb8c66c2e11a03e9b1470d688eb86f0bb93371
zhangli20080808 <791492434@qq.com>
2021-03-13 17:21:39 +0800

    feat: proxy

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,7 +1,35 @@
 (function () {
-   'use strict';
+    'use strict';
 
-
+    function initProxy(obj) {
+        var result = {};
+        var _loop_1 = function (key) {
+            result[key] = {
+                get: function () {
+                    console.log('get key 自定义逻辑');
+                    return obj[key];
+                },
+                set: function (value) {
+                    console.log('set');
+                    obj[key] = value;
+                }
+            };
+        };
+        for (var key in obj) {
+            _loop_1(key);
+        }
+        console.log(result, obj, 'res');
+        return result;
+    }
+    var person = {
+        name: 'zk',
+        age: 20
+    };
+    var testObj = initProxy(person);
+    console.log(testObj.name, 'name');
+    // testObj.name = '123'
+    // testObj.age = 10
+    console.log(testObj, 'result');
 
 }());
 //# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,54 @@
-/**
- * 内置工具类型
- * +? 变为可选  -? 变为必选   修饰符？+ -
- * 1. Partial可以将传入的属性由非可选变成可选
- * 2. Required
- * 3. Readonly
- */
-
-interface IProps {
-    a: string
-    b: number
-    c: boolean
+type Proxy<T> = {
+    get(): T,
+    set(value: T): void
+}
+type InitProxy<T> = {
+    [P in keyof T]: Proxy<T[P]>
 }
 
-/**
- *  很好理解 批量定义
- *  1. keyof T 拿到T对应key的所有集合
- *  2. 迭代T中的所有k 通过 P in keyof T
- *  type Partial<T> = {
-        [P in keyof T]?: T[P];
-    };
- */
-
-type A = Partial<IProps>
-const a: A = {
-    a: '123',
-    b: 1,
-    c: true
-}
-
-// 不过需要考虑嵌套的情况
-
-interface Company {
-    name: string
-    id: number
+function initProxy<T>(obj: T): InitProxy<T> {
+    let result = <InitProxy<T>>{}
+    for (const key in obj) {
+        result[key] = {
+            get: () => {
+                console.log('get key 自定义逻辑')
+                return obj[key]
+            },
+            set: (value) => {
+                console.log('set')
+                obj[key] = value
+            }
+        }
+    }
+    console.log(result,obj,'res')
+    return result
 }
 
 interface Person {
     name: string
-    id: number
-    company: Company
-}
-
-// 递归变成可选项
-type DeepPartial<T> = {
-    [U in keyof T]?: T[U] extends object ? DeepPartial<T[U]> : T[U]
-}
-
-type PersonPartial = DeepPartial<Person>
-// 如果传了 company 但是 company 没有传id 和 name 会报错 不过也可以处理 实现DeepPartial 将每个属性变成可选类型
-
-let p: PersonPartial = {
-    name: 'zl',
-    id: 12,
-    company: {}
-}
-
-// 把可选项变成必选项
-interface ATest {
-    name: string
-    id?: number
-}
-
-type RequireA = Required<ATest>
-const demo: RequireA = {
-    name: 'zl',
-    id: 1
-}
-
-// Pick 捡 只需要的属性
-interface Person2 {
-    name: string
-    id: number
     age: number
 }
 
-let result: Person2 = {name: 'zl', id: 1, age: 20}
-/**
- * type Pick<T, K extends keyof T> = {
-     [P in K]: T[P];
-   };
- */
+let person: Person = {
+    name: 'zk',
+    age: 20
+}
 
-type KeyOfPerson = keyof Person2  // "name" | "id" | "age"
-type ResultInfo = Pick<Person2, 'name' | 'id'>
-
-// Extract 有啥区别  对象中提取  类型提取
-// type Extract<T, U> = T extends U ? T : never;
-type E = Extract<string | number | boolean, string | number>
-let e: E = 1 //string
+let testObj: any = initProxy<Person>(person)
+console.log(testObj.name, 'name')
+// testObj.name = '123'
+// testObj.age = 10
+console.log(testObj, 'result')
 
 
-/**
- * Record
- * 将一个类型的所有属性值都映射到另一个类型上并创建新的类型
- */
+// function unProxy<T>(t: InitProxy<T>): T {
+//     let result: any = {} as T
+//     for (let key in t) {
+//         result[key] = t[key]
+//     }
+//     return result
+// }
+//
+// let originalPerson = unProxy<Person>(testObj)
+// console.log(originalPerson)
 export {}
